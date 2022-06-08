920854c7e36b493aa165d44cf7e12a2313809912
Imran Peerbhai <ipeerbhai@aol.com>
2022-06-08 00:15:39 -0700

    Adding instruction link and commands to mount a virtual drive as S. This will allow a better experience building a library.

---
+++ VirtualDrive/MountB2Bucket.ps1
@@ -0,0 +1,5 @@
+## to learn how to configure rclone, chocolatey see URL:
+## https://www.nakivo.com/blog/mount-amazon-s3-as-a-drive-how-to-guide/
+
+
+rclone mount b2-virtualdata:virtualdata/ S: --vfs-cache-mode full
