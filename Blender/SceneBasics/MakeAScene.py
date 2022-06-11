d0dba57964b5d629218c1f6559b531f506dcaa62
Imran Peerbhai <ipeerbhai@aol.com>
2022-06-10 19:12:32 -0700

    Adding simple change to pull local cache library sync'd from cloud, as a remote bucket is too slow for large file loads in blender.

--- Blender/SceneBasics/MakeAScene.py
+++ Blender/SceneBasics/MakeAScene.py
@@ -3,9 +3,15 @@
 ## Import my scene library to do some of the work.
 import importlib.util
 import sys
+from inspect import getsourcefile
+from os.path import abspath
+import os
+import pdb
 
-file_path = "C:/github/technicalsmartistry/Blender/SceneBasics/ImranSceneLib.py"
+github_drive = "f"
+file_path = github_drive + ":/github/technicalsmartistry/Blender/SceneBasics/ImranSceneLib.py"
 module_name = "ImranSceneLib"
+
 
 spec = importlib.util.spec_from_file_location(module_name, file_path)
 module = importlib.util.module_from_spec(spec)
@@ -13,12 +19,10 @@ sys.modules[module_name] = module
 spec.loader.exec_module(module)
 
 import ImranSceneLib as im
-import pdb
 
 ## Setup an importer and import the house, HDR, and treasure chest into the scene
 importer = im.Importers()
-chest, chestErr = importer.ImportGLTF("C:\\temp\\furniture\\chest", "treasure_chest_4k.gltf")
-pdb.set_trace()
-house, houseErr = importer.ImportFromBlendFile(blendFile="C:\\temp\\buildings", objectName="Cottage_FREE")
-importer.ImportHDRorEXRIntoWorld("C:\\temp\\EXRs\\je_gray_park_4k.hdr")
+chest, chestErr = importer.ImportGLTF("C:\\temp\\AssetLibrary\\furniture\\chest", "treasure_chest_4k.gltf")
+house, houseErr = importer.ImportFromBlendFile(blendFile="C:\\temp\\AssetLibrary\\buildings\\Cottage_FREE.blend", objectName="Cottage_Free")
+importer.ImportHDRorEXRIntoWorld("C:\\temp\\AssetLibrary\\EXRs\\je_gray_park_4k.hdr")
 
