49cd2cb8daa4dc7b977c56b90700f28aac5ec39e
Imran Peerbhai <ipeerbhai@aol.com>
2022-07-12 21:04:03 -0700

    First question has a rough answer -- I can create eyes and track an invisible object with it.  Doing a minor comment and cleanup pass.

--- Blender/AnimationBasics/AN_Questions.py
+++ Blender/AnimationBasics/AN_Questions.py
@@ -84,25 +84,23 @@ class MeshUtilities():
         bmesh.update_edit_mesh(dataItem)
         pass ## Just to look pretty
 
-    ## GetCenterPt takes a mesh, and gives you the mesh's box center.
-    def GetCenterPt(self, mesh):
-        pass
-
-
 ## A class to create meshes
 class MeshPrimitives():
+    ## Circle is a simple circle
     def Circle(self, radius=1, location=(0, 0, 0)):
         bpy.ops.mesh.primitive_circle_add(radius=radius, enter_editmode=False, align='WORLD', location=location, scale=(1, 1, 1))
         circle = bpy.context.object.data
         bpy.context.selected_objects[0].name = circle.name
         return(circle)
 
+    ## IvoShphere is a sphere with regularly placed vertices
     def IcoSphere(self, radius=1, location=(0, 0, 0)):
         bpy.ops.mesh.primitive_ico_sphere_add(radius=radius, enter_editmode=False, align='WORLD', location=location, scale=(1, 1, 1))
         sphere = bpy.context.object.data
         bpy.context.selected_objects[0].name = sphere.name
         return(sphere)
 
+    ## UVSphere is a sphere with vertices increasing in concentration around one axis.
     def UVSphere(self, radius=1, location=(0, 0, 0)):
         bpy.ops.mesh.primitive_uv_sphere_add(radius=radius, enter_editmode=False, align='WORLD', location=location, scale=(1, 1, 1))
         sphere = bpy.context.object.data
@@ -121,7 +119,6 @@ class SkeletonUtilities():
             if object.type == 'ARMATURE':
                 ## select the object
                 object.select_set(True)
-        
         world.DeleteSelected()
         pass
 
@@ -157,48 +154,59 @@ class TextureUtilities():
 
 ## Some basic utilites that apply to the basic world
 class WorldUtilities():
+
+    ## SetupWorld sets the world's unit system
     def SetupWorld(self, system='METRIC', unit='METERS'):
         bpy.context.scene.unit_settings.system = system
         bpy.context.scene.unit_settings.length_unit = unit
         pass
 
+    ## DeleteSelected deletes a selected object from the world
     def DeleteSelected(self):
         bpy.ops.object.delete(use_global=False, confirm=False)
         pass
 
+    ## DeselectAll selects nothing.
     def DeselectAll(self):
         bpy.ops.object.select_all(action='DESELECT')
         pass
 
+    ## SelectItems first deselects any other item, then selects the list of items.
     def SelectItems(self, items):
         self.DeselectAll()
         self.SelectAdditionalItems(items)
         pass
 
+    ## SelectAdditionalItems adds items to the selection
     def SelectAdditionalItems(self, items, scene='Scene'):
         for toselectItem in items:
             bpy.data.scenes[scene].objects.get(toselectItem.name).select_set(True)
         pass
 
+    ## SetSceneKeysToObjectDataNames sets the world outline name for an object to the same name as the objects data name.
     def SetSceneKeysToObjectDataNames(self, scene='Scene'):
         allObjects = bpy.data.scenes[scene].objects
         for object in allObjects:
             object.name = object.data.name
         pass
 
-    def SetSceneObjectDataNamesToSeneNames(self, scene='Scene'):
+    ## SetSceneObjectDataNamesToSceneNames sets the object's data name to the world outliner name.
+    def SetSceneObjectDataNamesToSceneNames(self, scene='Scene'):
         allObjects = bpy.data.scenes[scene].objects
         for object in allObjects:
             object.data.name = object.name
         pass
 
-    ## In blender, objects are typed.  The world/scene objects are a different type than the same object as a Mesh, bone, etc
+    ## GetWorldObjectFromObject gets the data object from a world object. In blender, objects are typed.  The world/scene objects are a different type than the same object as a Mesh, bone, etc
     def GetWorldObjectFromObject(self, object, scene='Scene'):
          target = bpy.data.scenes[scene].objects.get(object.name)
          return(target)
 
-
-
+    ## HideObjectFromRender hides an object from being rendered in a full render, but shows in viewport.
+    def HideObjectFromRender(self, object):
+        worldRef = self.GetWorldObjectFromObject(object)
+        worldRef.hide_render = True
+        pass
      
 ###
 ## Main Questions
@@ -227,38 +235,39 @@ class BasicAnimationQuestions():
         return(primBody)
 
 
-    ## How do I create an armiture and add a bone to a single mesh?
-    def HowDoIattachABoneToAUVSphere(self):
+    ## How do I use what I know to animate two eyes tracking something?
+    def HowDoIAnimateEyes(self):
         ## clear everything, draw the character, create a bone, and parent an eye to the created bone.
         self.meshUtils.DeleteAllMeshObjects()
         self.skelUtils.DeleteAllArmitureObjects()
         character = self.CreateHeadMeshes()
-        bone = self.skelUtils.AddArmitureToMesh(character['leftEye'])
-        self.worldUtils.SetSceneObjectDataNamesToSeneNames()
 
         ## Blender doesn't support points as meshes, so create a very small circle as a control point and aim the bone at the circle.  
         reticle = self.meshPrims.Circle(radius=0.01, location=(0, 0, 5))
         reticle.name = 'Reticle'
+        self.worldUtils.SetSceneKeysToObjectDataNames()
+        self.worldUtils.HideObjectFromRender(reticle)
 
-        ## update the world to match data names
-        #self.worldUtils.SetSceneKeysToObjectDataNames()
-
-        ## add a tracking constraint to the bone.
-        self.worldUtils.DeselectAll()
-        self.worldUtils.SelectItems([bone])
-        bpy.ops.outliner.item_activate(deselect_all=True)
-
-        #bpy.ops.object.constraint_add(type='TRACK_TO')
-        ## bpy.context.object.constraints["Track To"].target = self.worldUtils.GetWorldObjectFromObject(reticle)
-
-
-
-
-
-
-
+        ## add a bone to each eye and a tracking constraint to the bone.
+        eyes = ['leftEye', 'rightEye']
+        bones = []
+        for eye in eyes:
+            self.worldUtils.DeselectAll()
+            bone = self.skelUtils.AddArmitureToMesh(character[eye])
+            self.worldUtils.SetSceneKeysToObjectDataNames()
+            bpy.ops.object.constraint_add(type='TRACK_TO')
+            bpy.context.object.constraints["Track To"].target = self.worldUtils.GetWorldObjectFromObject(reticle)
+            bones.append(bone)
+        
+        ## update the character hash to include the bones.
+        character['bones'] = bones
         return(character)
+    
+    ## How do I squish a ball?
+    def HowdDoISquishABall(self):
+        pass
 
 ## run the question
 q = BasicAnimationQuestions()
-q.HowDoIattachABoneToAUVSphere()
+#q.HowDoIAnimateEyes()
+q.HowdDoISquishABall()
